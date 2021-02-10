d30aa6af1732ea14790cd423aee68900af1b27b4
max-y-huang <letmaxknow@gmail.com>
2021-02-10 01:55:39 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let router = express.Router();
+let axios = require('axios');
+
+const getData = async (id) => {
+  try {
+    let res = await axios.get(`https://www.conwaylife.com/patterns/${id}.cells`);
+    return res.data;
+  } catch (err) {
+    return null;
+  }
+}
+
+// Takes in an array of .s and Os. Returns it in array form.
+const convertToArray = (arr) => {
+  // Get number of columns by taking the maximum row length.
+  let cols = arr.reduce(((acc, val) => (val.length > acc) ? val.length : acc), 0);
+  return arr.map(row =>[
+    ...row.split('').map(cell => (cell === 'O') ? 1 : 0),
+    ...Array(cols - row.length).fill(0)
+  ]);
+}
+
+router.get('/:id', async (req, res, next) => {
+
+  let data = await getData(req.params.id);
+
+  if (!data) {
+    return res.status(400).json({ result: 'An error has occured :(' });
+  }
+
+  return res.status(200).json({
+    result:
+      convertToArray(
+        data.split('\r').join('')  // Remove all instances of '\r'.
+            .split('\n').slice(4)  // Split by '\n' and remove header.
+            .filter(x => x != '')  // Remove empty lines.
+      )
+  });
+});
+
+module.exports = router;
