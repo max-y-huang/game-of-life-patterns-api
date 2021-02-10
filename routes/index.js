efc9370c77f9b9495d5d97e6140bb021ea880e57
max-y-huang <letmaxknow@gmail.com>
2021-02-10 02:03:29 -0500

    renamed project, added comments

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 let axios = require('axios');
 
+// Gets the structure data from conwaylife.com.
 const getData = async (id) => {
   try {
     let res = await axios.get(`https://www.conwaylife.com/patterns/${id}.cells`);
@@ -16,8 +17,8 @@ const convertToArray = (arr) => {
   // Get number of columns by taking the maximum row length.
   let cols = arr.reduce(((acc, val) => (val.length > acc) ? val.length : acc), 0);
   return arr.map(row =>[
-    ...row.split('').map(cell => (cell === 'O') ? 1 : 0),
-    ...Array(cols - row.length).fill(0)
+    ...row.split('').map(cell => (cell === 'O') ? 1 : 0),  // Convert . to 0 and O to 1.
+    ...Array(cols - row.length).fill(0)                    // Fill remaining cells with 0s.
   ]);
 }
 
